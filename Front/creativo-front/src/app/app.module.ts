2aa1120f76591acd8bee48ca2972719f687a4703
andyrrr <43052958+andyrrr@users.noreply.github.com>
2024-02-03 15:37:48 -0600

    routing

--- Front/creativo-front/src/app/app.module.ts
+++ Front/creativo-front/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { GestionAdministradoresComponent } from './gestion-administradores/gestion-administradores.component';
-import { GestionSolicitudesComponent } from './gestion-solicitudes/gestion-solicitudes.component';
-import { RegistroSolicitudComponent } from './registro-solicitud/registro-solicitud.component';
-import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
+import { GestionAdministradoresComponent } from './Administrador/gestion-administradores/gestion-administradores.component';
+import { GestionSolicitudesComponent } from './Administrador/gestion-solicitudes/gestion-solicitudes.component';
+import { RegistroSolicitudComponent } from './Emprendimiento/registro-solicitud/registro-solicitud.component';
+import { PreguntasFrecuentesComponent } from './General/preguntas-frecuentes/preguntas-frecuentes.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,16 @@ import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-f
     PreguntasFrecuentesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot([
+      {path: "", component:PreguntasFrecuentesComponent},
+      {path: "preguntas", component:PreguntasFrecuentesComponent},
+      {path: "solicitudes", component:GestionSolicitudesComponent},
+      {path: "administradores", component:GestionAdministradoresComponent},
+      {path: "solicitud", component:RegistroSolicitudComponent},
+    ]),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
